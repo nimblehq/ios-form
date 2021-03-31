0109622ce56672efc9b7ffa0f380bd3e186f67e9
Su Ho <su@nimblehq.co>
2021-03-31 16:58:11 +0700

    [feature] Update logic to getting data from form

--- Sources/Modules/Form/FormDemoViewModel.swift
+++ Sources/Modules/Form/FormDemoViewModel.swift
@@ -14,6 +14,88 @@ final class FormDemoViewModel {
     init(router: FormDemoRouter) {
         self.router = router
     }
+
+    private func configureProvinceAfterSelectCountry() {
+        guard let view = view else { return }
+        guard
+            let country = view.dataSource.getValue(
+                of: SelectInputFormField<Country>.self,
+                byKey: FormFieldKey.country()
+            )
+        else { return }
+        if country.title == "Thailand" {
+            view.dataSource.updateDataSource(
+                for: SelectInputFormField<Province>.self,
+                with: Province.thaiProvinces,
+                byKey: FormFieldKey.province()
+            )
+        } else {
+            view.dataSource.updateDataSource(
+                for: SelectInputFormField<Province>.self,
+                with: Province.vietProvinces,
+                byKey: FormFieldKey.province()
+            )
+        }
+        view.dataSource.updateValue(
+            for: SelectInputFormField<Province>.self,
+            with: nil,
+            byKey: FormFieldKey.province()
+        )
+    }
+
+    private func handleEnabled2FA() {
+        guard let view = view else { return }
+        let enabled2FA = view.dataSource.getValue(
+            of: ToggleInputFormField.self,
+            byKey: FormFieldKey.enabled2FA()
+        )
+        if enabled2FA {
+            if !view.dataSource.fields.contains(where: { $0.key == FormFieldKey.twoFA() }) {
+                let fields = view.dataSource.fields(ofSection: FormSectionKey.profile())
+                view.dataSource.insertField(
+                    TextInputFormField(key: FormFieldKey.twoFA(), viewModel: .init(title: "2FA Code")),
+                    at: fields.count,
+                    ofSection: FormSectionKey.profile()
+                )
+            }
+        } else {
+            if view.dataSource.fields.contains(where: { $0.key == FormFieldKey.twoFA() }) {
+                view.dataSource.removeField(FormFieldKey.twoFA())
+            }
+        }
+    }
+
+    private func handleEnabledAddress() {
+        guard let view = view else { return }
+        let enabledAddress = view.dataSource.getValue(
+            of: ToggleInputFormField.self,
+            byKey: FormFieldKey.enabledAddress()
+        )
+        if enabledAddress {
+            let addressSection = FormSection(
+                key: FormSectionKey.address(),
+                fields: [
+                    SelectInputFormField<Country>(
+                        key: FormFieldKey.country(),
+                        viewModel: .init(title: "Country"),
+                        dataSource: Country.list,
+                        router: router
+                    ),
+                    SelectInputFormField<Province>(
+                        key: FormFieldKey.province(),
+                        viewModel: .init(title: "Province"),
+                        router: router
+                    )
+                ]
+            )
+            addressSection.fields.forEach {
+                $0.delegate = self
+            }
+            view.dataSource.insertSection(addressSection, at: view.dataSource.sections.count)
+        } else {
+            view.dataSource.removeSection(withKey: FormSectionKey.address())
+        }
+    }
 }
 
 // MARK: - FormDemoViewOutput
@@ -55,25 +137,36 @@ extension FormDemoViewModel: FormDemoViewOutput {
     func didTapSaveButton() {
         guard let view = view else { return }
         var message = ""
+        let fullName = view.dataSource.getValue(of: TextInputFormField.self, byKey: FormFieldKey.fullName())
+        message += "fullName: \(fullName)\n"
         let username = view.dataSource.getValue(of: TextInputFormField.self, byKey: FormFieldKey.username())
         message += "username: \(username)\n"
         let password = view.dataSource.getValue(of: TextInputFormField.self, byKey: FormFieldKey.password())
         message += "password: \(password)\n"
         let enabled2FA = view.dataSource.getValue(of: ToggleInputFormField.self, byKey: FormFieldKey.enabled2FA())
         message += "enabled2FA: \(enabled2FA)\n"
-        let fullName = view.dataSource.getValue(of: TextInputFormField.self, byKey: FormFieldKey.fullName())
-        message += "fullName: \(fullName)\n"
-        if let country = view.dataSource.getValue(
-            of: SelectInputFormField<Country>.self,
-            byKey: FormFieldKey.country()
-        ) {
-            message += "country: \(country.title)\n"
+        if enabled2FA {
+            let twoFA = view.dataSource.getValue(of: TextInputFormField.self, byKey: FormFieldKey.twoFA())
+            message += "2FA: \(twoFA)\n"
         }
-        if let province = view.dataSource.getValue(
-            of: SelectInputFormField<Province>.self,
-            byKey: FormFieldKey.province()
-        ) {
-            message += "province: \(province.title)\n"
+        let enabledAddress = view.dataSource.getValue(
+            of: ToggleInputFormField.self,
+            byKey: FormFieldKey.enabledAddress()
+        )
+        message += "enabledAddress: \(enabledAddress)\n"
+        if enabledAddress {
+            if let country = view.dataSource.getValue(
+                of: SelectInputFormField<Country>.self,
+                byKey: FormFieldKey.country()
+            ) {
+                message += "country: \(country.title)\n"
+            }
+            if let province = view.dataSource.getValue(
+                of: SelectInputFormField<Province>.self,
+                byKey: FormFieldKey.province()
+            ) {
+                message += "province: \(province.title)\n"
+            }
         }
         router.presentAlert(with: message)
     }
@@ -85,78 +178,11 @@ extension FormDemoViewModel: FormFieldDelegate {
         guard let view = view else { return }
         switch field.key {
         case FormFieldKey.country.rawValue:
-            guard
-                let country = view.dataSource.getValue(
-                    of: SelectInputFormField<Country>.self,
-                    byKey: FormFieldKey.country()
-                )
-            else { return }
-            if country.title == "Thailand" {
-                view.dataSource.updateDataSource(
-                    for: SelectInputFormField<Province>.self,
-                    with: Province.thaiProvinces,
-                    byKey: FormFieldKey.province()
-                )
-            } else {
-                view.dataSource.updateDataSource(
-                    for: SelectInputFormField<Province>.self,
-                    with: Province.vietProvinces,
-                    byKey: FormFieldKey.province()
-                )
-            }
-            view.dataSource.updateValue(
-                for: SelectInputFormField<Province>.self,
-                with: nil,
-                byKey: FormFieldKey.province()
-            )
+            configureProvinceAfterSelectCountry()
         case FormFieldKey.enabled2FA.rawValue:
-            let enabled2FA = view.dataSource.getValue(
-                of: ToggleInputFormField.self,
-                byKey: FormFieldKey.enabled2FA()
-            )
-            if enabled2FA {
-                if !view.dataSource.fields.contains(where: { $0.key == FormFieldKey.twoFA() }) {
-                    let fields = view.dataSource.fields(ofSection: FormSectionKey.profile())
-                    view.dataSource.insertField(
-                        TextInputFormField(key: FormFieldKey.twoFA(), viewModel: .init(title: "2FA Code")),
-                        at: fields.count,
-                        ofSection: FormSectionKey.profile()
-                    )
-                }
-            } else {
-                if view.dataSource.fields.contains(where: { $0.key == FormFieldKey.twoFA() }) {
-                    view.dataSource.removeField(FormFieldKey.twoFA())
-                }
-            }
+            handleEnabled2FA()
         case FormFieldKey.enabledAddress.rawValue:
-            let enabledAddress = view.dataSource.getValue(
-                of: ToggleInputFormField.self,
-                byKey: FormFieldKey.enabledAddress()
-            )
-            if enabledAddress {
-                let addressSection = FormSection(
-                    key: FormSectionKey.address(),
-                    fields: [
-                        SelectInputFormField<Country>(
-                            key: FormFieldKey.country(),
-                            viewModel: .init(title: "Country"),
-                            dataSource: Country.list,
-                            router: router
-                        ),
-                        SelectInputFormField<Province>(
-                            key: FormFieldKey.province(),
-                            viewModel: .init(title: "Province"),
-                            router: router
-                        )
-                    ]
-                )
-                addressSection.fields.forEach {
-                    $0.delegate = self
-                }
-                view.dataSource.insertSection(addressSection, at: view.dataSource.sections.count)
-            } else {
-                view.dataSource.removeSection(withKey: FormSectionKey.address())
-            }
+            handleEnabledAddress()
         default: break
         }
     }
